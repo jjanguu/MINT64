9fd69df7e930fee27bff7b221e9ef7aa373d77b8
jjanguu <tk3414@naver.com>
2022-11-08 17:37:52 +0900

    chap8 clear

--- 01.Kernel32/Source/Main.c
+++ 01.Kernel32/Source/Main.c
@@ -1,11 +1,34 @@
 #include "Types.h"
 
 void kPrintString( int iX, int iY, const char* pcString );
+BOOL kInitializeKernel64Area();
+BOOL kIsMemoryEnough();
 
 void Main( void ){
-    kPrintString(0, 3, "C Language Kernel Started~!!!" );
+    DWORD i;
 
-    while ( 1 );
+    kPrintString(0, 3, "C Language Kernel Start.....................[Pass]" );
+    kPrintString(0, 4, "Minimum Memory Size Check...................[    ]");
+
+    if(kIsMemoryEnough() == FALSE){
+        kPrintString(45, 4, "Fail");
+        kPrintString(0, 5, "Not Enough Memory~!! MINT64 OS Requires Over 64Mbyte Memory~!!");
+        while(1);
+    } else{
+        kPrintString(45, 4,"Pass");
+    }
+
+    kPrintString(0, 5, "IA-32e Kernel Area Initialize...............[    ]");
+
+    if(kInitializeKernel64Area() == FALSE){
+        kPrintString(45, 5, "Fail");
+        kPrintString(0, 6, "Kernel Area Initialization Fail~!!");
+        while(1);
+    }
+    
+    kPrintString(45, 5, "Pass");
+
+    while(1);
 }
 
 void kPrintString( int iX, int iY, const char* pcString){
@@ -16,4 +39,38 @@ void kPrintString( int iX, int iY, const char* pcString){
     for( i=0; pcString[i] !=0; i++){
         pstScreen[i].bCharactor = pcString[i];
     }
+}
+
+BOOL kInitializeKernel64Area(){
+    DWORD* pdwCurrentAddress;
+
+    pdwCurrentAddress = (DWORD*) 0x100000;
+
+    while((DWORD)pdwCurrentAddress < 0x600000){
+        *pdwCurrentAddress = 0x00;
+
+        if (*pdwCurrentAddress != 0){
+            return FALSE;
+        }
+
+        pdwCurrentAddress++;
+    }
+    return TRUE;
+}
+
+BOOL kIsMemoryEnough(){
+    DWORD* pdwCurrentAddress;
+
+    pdwCurrentAddress = (DWORD*) 0x100000;
+
+    while((DWORD)pdwCurrentAddress < 0x4000000){
+        *pdwCurrentAddress = 0x12345678;
+
+        if(*pdwCurrentAddress != 0x12345678){
+            return FALSE;
+        }
+
+        pdwCurrentAddress += (0x100000/4);
+    }
+    return TRUE;
 }
